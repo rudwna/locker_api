70694e8d3094a2afc705946fc685fdc348907fe8
Rud Wangrungarun <rudwna@gmail.com>
2015-06-05 03:06:27 +0000

    Change postgresql client's variable to pgc, remove testing functions

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ io.sockets.on('connection', function(socket) {
 
 
 
-pg.connect(pg_constr, function(err, client, done) {
+pg.connect(pg_constr, function(err, pgc, done) {
 	if (err) {
-		return console.error('error fetching client from pool', err);
+		return console.error('error fetching pgc from pool', err);
 	}
 
 
@@ -49,12 +49,12 @@ pg.connect(pg_constr, function(err, client, done) {
 		if (can_cmd == 0x100) {
 			// locker registration request
 			// check if msg.data[0] (LID) is in database
-			client.query('select id, logical_id, state from locker where id = $1', [locker_id], function(err, result) {
+			pgc.query('select id, logical_id, state from locker where id = $1', [locker_id], function(err, result) {
 				var locker = result.rows[0];
 
 				// if there's no data, create new row for it
 				if (result.rows[0] === undefined) {
-					client.query('insert into locker(id, state) values ($1, $2)', [locker_id, 0], function(err, result) {
+					pgc.query('insert into locker(id, state) values ($1, $2)', [locker_id, 0], function(err, result) {
 						if (!err) {
 							var regd = { id: 0x180 + locker_id, ext: false, data: new Buffer([]) };
 							can_ch.send(regd);
@@ -82,12 +82,13 @@ pg.connect(pg_constr, function(err, client, done) {
 				}
 			});
 		} else if (msg.id == 0x300) {
-			var uid = msg.data.toString('hex');
+			var user_id = msg.data.toString('hex');
+			console.log(user_id);
 
 			var query = 	"select id from locker where id = ( \
 								select locker_id from reservation where id = ( \
 									select max(id) from reservation where personal_id = ( \
-										select id from locker_user where encode(rfid_uid, 'hex') = $1 \
+										select id from locker_user where username = $1 \
 									) \
 								group by locker_id \
 								) \
@@ -95,20 +96,20 @@ pg.connect(pg_constr, function(err, client, done) {
 							and (state = 1 or state = 3)";
 
 
-			client.query(query, [uid], function(err, result_i) {
+			pgc.query(query, [user_id], function(err, result_i) {
 				// console.log(result_i);
 				if (undefined === result_i.rows[0]) {
 					return;
 				}
 				var locker_id = result_i.rows[0].id;
-				client.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id=$1)", [locker_id], function(err, result_ii) {
+				pgc.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id=$1)", [locker_id], function(err, result_ii) {
 					console.log(result_ii.rows);
 
 					if (!result_ii.rows[0].is_activated) {
-						client.query("UPDATE reservation SET is_activated = 't' WHERE id = $1", [result_ii.rows[0].id]);
+						pgc.query("UPDATE reservation SET is_activated = 't' WHERE id = $1", [result_ii.rows[0].id]);
 					}
 
-					client.query("UPDATE locker SET state = 2 WHERE id=$1", [locker_id], function(err, result_iii) {
+					pgc.query("UPDATE locker SET state = 2 WHERE id=$1", [locker_id], function(err, result_iii) {
 						// TODO: catch err
 						// if (err) throw err;
 						// console.log('locker ' + locker_id + ' opened');
@@ -121,26 +122,30 @@ pg.connect(pg_constr, function(err, client, done) {
 			})
 
 		} else if (can_cmd == 0x400) {
-			client.query('select id, logical_id, state from locker where state=2 and id=$1', [locker_id], function(err, result) {
+			pgc.query('select id, logical_id, state from locker where state=2 and id=$1', [locker_id], function(err, result) {
 				var locker = result.rows[0];
 
-				client.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id = $1)", [locker_id], function(err, result_i) {
+				if (result.rows.length == 0) {
+					return;
+				}
+
+				pgc.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id = $1)", [locker_id], function(err, result_i) {
 					if (err) throw err;
 
 					if (result_i.rows.length == 0) {
 						return;
 					}
 
+					console.log('s'+ locker.state);
+
 					if (2 != locker.state) {
-						// throw "Invalid closing state";
+						throw "Invalid closing state";
 						return;
 					} else {
-						client.query("UPDATE locker SET state = 3 WHERE id=$1", [locker_id], function(err, result) {
+						pgc.query("UPDATE locker SET state = 3 WHERE id=$1", [locker_id], function(err, result) {
 							if (err) throw err;
 
 							console.log('locker ' + locker_id + ' closed');
-							// var canmsg = { id: 0x400 + locker_id, ext: false, data: new Buffer([]) };
-							// can_ch.send(canmsg);
 						});
 					}
 				});
@@ -156,7 +161,7 @@ pg.connect(pg_constr, function(err, client, done) {
 		}
 
 
-		client.query('select id from locker_user where username = $1 and password = $2', [username, sha1(password)], function(err, result) {
+		pgc.query('select id from locker_user where username = $1 and password = $2', [username, sha1(password)], function(err, result) {
 			if (err) {
 				throw err;
 			}
@@ -177,55 +182,17 @@ pg.connect(pg_constr, function(err, client, done) {
 
 
 	server.get('/test', function(req, res, next) {
-		client.query('select id, logical_id, state from locker where id = 8', function(err, result) {
+		pgc.query('select id, logical_id, state from locker where id = 8', function(err, result) {
 			res.send(result.rows[0]);
 		});
 	});
 
 
-	server.get('/qx', function(req, res, next) {
-		var uid = '8302372e9800';
-
-		var query = 	"select id from locker where id = ( \
-							select locker_id from reservation where id = ( \
-								select max(id) from reservation where personal_id = ( \
-									select id from locker_user where encode(rfid_uid, 'hex') = $1 \
-								) \
-							group by locker_id \
-							) \
-						) \
-						and (state = 1 or state = 3)";
-
-		client.query(query, [uid], function(err, result_i) {
-			console.log(result_i);
-			var locker_id = result_i.rows[0].id;
-			client.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id=$1)", [locker_id], function(err, result_ii) {
-				console.log(result_ii.rows);
-
-				if (!result_ii.rows[0].is_activated) {
-					client.query("UPDATE reservation SET is_activated = 't' WHERE id = $1", [result_ii.rows[0].id]);
-				}
-
-				client.query("UPDATE locker SET state = 2 WHERE id=$1", [locker_id], function(err, result_iii) {
-					// TODO: catch err
-					console.log('yyy');
-					// if (err) throw err;
-					// console.log('locker ' + locker_id + ' opened');
-					res.send(200);
-
-					var canmsg = { id: 0xA, ext: false, data: new Buffer([locker_id]) };
-					can_ch.send(canmsg);
-				});
-
-			});
-		})
-
-	});
 
 	server.get('/user/is_reserved', function(req, res, next) {
 		var user_id = req.user.id;
 
-		client.query('select l.logical_id from reservation r, locker l where r.id in ( \
+		pgc.query('select l.logical_id from reservation r, locker l where r.id in ( \
 			select max(id) from reservation group by locker_id \
 			) and l.state > 0 and personal_id = $1 and l.id = r.locker_id', [user_id], function(err, result) {
 			res.send({ is_reserved: result.rows.length > 0 ? result.rows[0].logical_id : 'false'});
@@ -236,7 +203,7 @@ pg.connect(pg_constr, function(err, client, done) {
 		var user_id = req.user.id;
 		var page = req.params['page'] || 0;
 
-		client.query("select a.id, a.created, a.state, a.reservation_id, l.logical_id from activity a, reservation r, locker l \
+		pgc.query("select a.id, a.created, a.state, a.reservation_id, l.logical_id from activity a, reservation r, locker l \
 			where a.reservation_id in (select r.id from reservation r where r.personal_id = $1 order by id desc limit 5 offset $2) \
 				and a.reservation_id = r.id and r.locker_id = l.id \
 			order by a.id asc", [user_id, 5*page], function(err, result) {
@@ -247,13 +214,8 @@ pg.connect(pg_constr, function(err, client, done) {
 		})
 	});
 
-	server.get('/echo/:name', function (req, res, next) {
-		res.send(req.params);
-		return next();
-	});
-
 	server.get('/lockers', function(req, res, next) {
-		client.query('select * from locker where logical_id is not null order by logical_id asc', function(err, result) {
+		pgc.query('select * from locker where logical_id is not null order by logical_id asc', function(err, result) {
 			if (err) {
 				console.log(err);
 				return;
@@ -266,7 +228,7 @@ pg.connect(pg_constr, function(err, client, done) {
 	server.post('/lockers/:logical_id/reserve', function(req, res) {
 		var user_id = req.user.id;
 		var logical_id = req.params['logical_id'];
-		client.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
+		pgc.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
 			var locker_id = result.rows[0].id;
 			var state = result.rows[0].state;
 
@@ -274,7 +236,7 @@ pg.connect(pg_constr, function(err, client, done) {
 				res.send(401, { code: 'R001', message: 'สถานะในการจองไม่ถูกต้อง' });
 			} else {
 				// TODOL validating data first
-				client.query('insert into reservation(locker_id, personal_id) values ($1, $2)',
+				pgc.query('insert into reservation(locker_id, personal_id) values ($1, $2)',
 						[locker_id, user_id], function(err, result) {
 							if (err) {
 								console.log(err);
@@ -296,22 +258,22 @@ pg.connect(pg_constr, function(err, client, done) {
 	server.post('lockers/:logical_id/open', function(req, res) {
 		var logical_id = req.params['logical_id'];
 
-		client.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
+		pgc.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
 			var locker_id = result.rows[0].id;
 			var state = result.rows[0].state;
 
 			if (state != 1 && state != 3) {
 				res.send(401, { code: 'R002', message: 'สถานะในการเปิดไม่ถูกต้อง' });
 			} else {
-				client.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id=$1)", [locker_id], function(err, result) {
+				pgc.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id=$1)", [locker_id], function(err, result) {
 					if (err) throw err;
 					console.log(result.rows);
 
 					if (!result.rows[0].is_activated) {
-						client.query("UPDATE reservation SET is_activated = 't' WHERE id = $1", [result.rows[0].id]);
+						pgc.query("UPDATE reservation SET is_activated = 't' WHERE id = $1", [result.rows[0].id]);
 					}
 
-					client.query("UPDATE locker SET state = 2 WHERE id=$1", [locker_id], function(err, result) {
+					pgc.query("UPDATE locker SET state = 2 WHERE id=$1", [locker_id], function(err, result) {
 						// TODO: catch err
 						if (err) throw err;
 						// console.log('locker ' + locker_id + ' opened');
@@ -330,37 +292,37 @@ pg.connect(pg_constr, function(err, client, done) {
 	// This method is not to be used by normal user
 	// user'll typically open and close locker by RFID authorization
 	// which sen CAN message the controller
-	server.post('lockers/:logical_id/close', function(req, res) {
-		var logical_id = req.params['logical_id'];
-		client.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
-			var locker_id = result.rows[0].id;
-			var state = result.rows[0].state;
+	// server.post('lockers/:logical_id/close', function(req, res) {
+	// 	var logical_id = req.params['logical_id'];
+	// 	pgc.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
+	// 		var locker_id = result.rows[0].id;
+	// 		var state = result.rows[0].state;
 
-			client.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id = $1)", [locker_id], function(err, result) {
-				if (err) throw err;
+	// 		pgc.query("SELECT id, is_activated FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id = $1)", [locker_id], function(err, result) {
+	// 			if (err) throw err;
 
-				if (2 != state) {
-					res.send(400, {'code': 'R003', 'message': 'สถานะในการปิดไม่ถูกต้อง'});
-				} else {
-					client.query("UPDATE locker SET state = 3 WHERE id=$1", [locker_id], function(err, result) {
-						if (err) throw err;
+	// 			if (2 != state) {
+	// 				res.send(400, {'code': 'R003', 'message': 'สถานะในการปิดไม่ถูกต้อง'});
+	// 			} else {
+	// 				pgc.query("UPDATE locker SET state = 3 WHERE id=$1", [locker_id], function(err, result) {
+	// 					if (err) throw err;
 
-						console.log('locker ' + locker_id + ' closed');
-						res.send(200);
+	// 					console.log('locker ' + locker_id + ' closed');
+	// 					res.send(200);
 
-						var canmsg = { id: 0x400 + locker_id, ext: false, data: new Buffer([]) };
-						can_ch.send(canmsg);
-					});
-				}
+	// 					var canmsg = { id: 0x400 + locker_id, ext: false, data: new Buffer([]) };
+	// 					can_ch.send(canmsg);
+	// 				});
+	// 			}
 
-			});
-		});
-		// return next();
-	});
+	// 		});
+	// 	});
+	// 	// return next();
+	// });
 
 	server.post('lockers/:logical_id/release', function(req, res) {
 		var logical_id = req.params['logical_id'];
-		client.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
+		pgc.query('select id, state from locker where is_alive=true and logical_id = $1', [logical_id], function(err, result) {
 			if (err) throw err;
 			var locker_id = result.rows[0].id;
 			var state = result.rows[0].state;
@@ -370,11 +332,11 @@ pg.connect(pg_constr, function(err, client, done) {
 			} else {
 				// conn.query("SELECT count(*) as count FROM reservation WHERE id IN (select max(id) from reservation group by locker_id) and personal_id=? group by personal_id", [personal_id], function(err, rows, result) {
 				// TODO: using dynamic id from RFID card
-				client.query("SELECT id FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id=$1)", [locker_id], function(err, rows, result) {
+				pgc.query("SELECT id FROM reservation WHERE id = (SELECT max(id) FROM reservation WHERE locker_id=$1)", [locker_id], function(err, rows, result) {
 					// TODO: catch err
 					if (err) throw err;
 
-					client.query("UPDATE locker SET state = 4 WHERE id = $1", [locker_id], function(err, result) {
+					pgc.query("UPDATE locker SET state = 4 WHERE id = $1", [locker_id], function(err, result) {
 						// TODO: catch err
 						if (err) throw err;
 
@@ -393,7 +355,7 @@ pg.connect(pg_constr, function(err, client, done) {
 
 
 	// pgsql LISTEN/NOTIFY
-	client.on('notification', function(msg) {
+	pgc.on('notification', function(msg) {
 		// console.log('notification');
 		// console.log(msg);
 		if (msg.channel == 'locker_is_alive_update') {
@@ -415,8 +377,8 @@ pg.connect(pg_constr, function(err, client, done) {
 		}
 	});
 
-	client.query('LISTEN locker_is_alive_update');
-	client.query('LISTEN locker_state_update');
+	pgc.query('LISTEN locker_is_alive_update');
+	pgc.query('LISTEN locker_state_update');
 
 });
 
